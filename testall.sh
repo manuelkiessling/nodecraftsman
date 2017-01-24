1ff31a8a880716ab8f9837b093df841ac83cd7e1
Manuel Kiessling <manuel@kiessling.net>
2017-01-24 09:44:19 +0100

    Added chapter index to folder names.

--- testall.sh
+++ testall.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-cd ./test-driven_nodejs_development
+cd ./ch01_test-driven_nodejs_development
 rm -rf node_modules
 docker run --rm -ti -v "$PWD":/opt/source -w /opt/source node:7.4.0 /bin/bash ./test.sh
 cd -
 
-cd ./building_a_complete_web_application_with_nodejs_and_angularjs
+cd ./ch10_building_a_complete_web_application_with_nodejs_and_angularjs
 rm -rf node_modules
 rm -rf src/frontend/bower_components
 docker stop bacwawnaa-mysql
